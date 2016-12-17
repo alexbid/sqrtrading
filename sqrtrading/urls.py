568df1e5a9547de19cfe65850757f13b494618f7
Alex <alex@Alexs-MacBook.local>
2016-12-17 17:44:29 +0100

    first manual feeding

--- sqrtrading/urls.py
+++ sqrtrading/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
                url(r'^stockscreener/', include('stockscreener.urls')),
                url(r'^admin/', admin.site.urls),
-               url(r'^$', include('stockscreener.urls')),
+               url(r'^$', lambda r: HttpResponseRedirect('stockscreener/')),
+#               url(r'^$', include('stockscreener.urls')),
+#               url(r'^$', stockscreener.views.index, name='index'),
 ]
