f041cd8303cccf7b21c1ca5af705470dd1de1cfa
Alex <alex@Alexs-MacBook.local>
2016-12-17 01:00:08 +0100

    first manual feeding

--- sqrtrading/settings.py
+++ sqrtrading/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '*$!%slbcrrkq_&xrl2-5g7h9p_avji)ko%1$jj5*c5o$)j2%1a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['www.sqrtrading.com', 'sqrtrading.com']
 
 # Application definition
 
