3e14358ee7a40fb47bd1c293434698742d632081
alexbid <bidault@hotmail.fr>
2017-03-16 21:18:50 +0100

    from my SnowLeopard

--- stockscreener/models.py
+++ stockscreener/models.py
@@ -5,28 +5,6 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.utils import timezone
 
 
-#@python_2_unicode_compatible  # only if you need to support Python 2
-#class Question(models.Model):
-#    question_text = models.CharField(max_length=200)
-#    pub_date = models.DateTimeField('Date')
-#
-#    def __str__(self):
-#        return self.question_text
-#
-#    def was_published_recently(self):
-#        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-#
-#
-#@python_2_unicode_compatible  # only if you need to support Python 2
-#class Choice(models.Model):
-#    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#    choice_text = models.CharField(max_length=200)
-#    votes = models.IntegerField(default=0)
-#
-#    def __str__(self):
-#        return self.choice_text
-
-
 @python_2_unicode_compatible  # only if you need to support Python 2
 class signals(models.Model):
     Date = models.DateField()
@@ -64,7 +42,7 @@ class batch_run(models.Model):
     mnemo = models.CharField(max_length=12)
 
     def __str__(self):
-        return str(self.BBG) + "\t" + str(self.CDR)+ "\t" + str(self.web_source)+ "\t" + str(self.IDX)+ "\t" + str(self.isWorking)
+        return str(self.BBG) + "" + str(self.CDR)+ "\t" + str(self.web_source)+ "\t" + str(self.IDX)+ "\t" + str(self.isWorking)
 
 
 
